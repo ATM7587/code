6e914ca265dd73ae8ed7da21c9736d534b5fe5d3
seotaeyoung <sty4725@naver.com>
2021-12-02 15:24:28 +0900

    파일 읽고 쓰기

--- lotto.py
+++ lotto.py
@@ -2,6 +2,8 @@ import random
 
 order  = list(input("사람 이름을 입력해 주세요(띄어쓰기로 구분) : ").split())
 
+print(f'{order}')
+
 random.shuffle(order)
 
 print(f'{order}')
--- python_basic/chapter09_01.py
+++ python_basic/chapter09_01.py
@@ -23,3 +23,58 @@ cts = f.read()
 print(cts)
 # 반드시 close
 f.close()
+
+# 예제2
+with open('./resource/it_news.txt', 'r', encoding='UTF-8') as f:
+    c = f.read()
+    print(c)
+    print(iter(c))
+    print(list(c))
+
+print()
+
+# 예제3
+# read() : 전체 읽기, read(10) : 10Byte씩 읽어옴
+
+with open('./resource/it_news.txt', 'r', encoding='UTF-8') as f:
+    c = f.read(20)
+    print(c)
+    c = f.read(20)
+    print(c) # 마지막에 어디까지 읽었는지를 기억ㅎ하는 커서가 존재함
+    c = f.read(20)
+    print(c)
+    f.seek(0,0)
+    c = f.read(20)
+    print(c)
+
+print()
+
+# 예제4
+# readline : 한 줄씩 읽기
+
+with open('./resource/it_news.txt', 'r', encoding='UTF-8') as f:
+    line = f.readline()
+    print(line)
+    line = f.readline()
+    print(line)
+
+print()
+
+# 예제5
+# readlines : 전체를 읽은 후 라인 단위 리스트로 저장
+
+with open('./resource/it_news.txt', 'r', encoding='UTF-8') as f:
+    cts = f.readlines()
+    print(cts)
+    print()
+    for c in cts:
+        print(c, end='')
+
+print()
+
+
+# 파일 쓰기(write)
+
+with open('./resource/contents1.txt', 'w') as f:
+    f.write('I love python\n')
+
